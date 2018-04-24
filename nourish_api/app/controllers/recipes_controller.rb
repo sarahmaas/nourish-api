a21cf5f94524a5368fcc88a8a0a42a03c70d5b54
Sarah Maas <sarah.ann.maas@gmail.com>
2018-04-23 23:01:26 -0500

    recipe controller set up except for user id references

--- nourish_api/app/controllers/recipes_controller.rb
+++ nourish_api/app/controllers/recipes_controller.rb
@@ -1,2 +1,44 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show, :update, :destroy]
+
+  # GET /recipes
+  def index
+    @recipes = recipe.all
+    render json: @recipes, status: :ok
+  end
+
+  # POST /recipes
+  def create
+    @recipe = Recipe.create!(recipe_params)
+    render json: @recipe, status:  :created
+  end
+
+  # GET /recipes/:id
+  def show
+    render json: @recipe, status: :ok
+  end
+
+  # PUT /recipes/:id
+  def update
+    @recipe.update(recipe_params)
+    head :no_content
+  end
+
+  # DELETE /recipes/:id
+  def destroy
+    @recipe.destroy
+    head :no_content
+  end
+
+  private
+
+  def recipe_params
+    # whitelist params
+    params.permit(:title, :user_id, :source, :servings, :description)
+  end
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
 end
